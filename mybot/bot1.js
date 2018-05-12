cce732da630d6ef7d0e2e46140e17e610eb6d1f1
guillaume <guillaume.landriu@free.fr>
2018-05-12 18:55:11 +0200

    ajout db

--- mybot/bot1.js
+++ mybot/bot1.js
@@ -59,75 +59,89 @@ This bot demonstrates many of the core features of Botkit:
 var Botkit = require('botkit');
 var os = require('os');
 var controller = Botkit.consolebot({
-    debug: true,
+    debug: false,
 });
 
 var bot = controller.spawn();
 var Datastore = require('nedb')
 
 db = {};
-db.intents = new Datastore({filename:'./db/intents.db',autoload:true});
-db.entities = new Datastore({filename:'./db/entities.db', autoload:true});
+db.intents = new Datastore({ filename: './db/intents.db', autoload: true });
+db.entities = new Datastore({ filename: './db/entities.db', autoload: true });
 
 let intents = [];
 
-var searchIntent = function(message) {
+var searchIntent = function (message) {
     db.intents.find({})
 }
 
+
+/**
+ * REcherche une intent.
+ * TODO gérer le mode asynchrone.
+ * @param {*} message 
+ */
 var checkIntent = function (message) {
     result = [];
-    intents.forEach((intent, index, array) => {
-        intent['examples'].forEach((example, ind, arr) => {
-            if (example == message) {
-                var resultIntent = {
-                    'intent': intent,
-                    'exact': true,
-                    'nbWordsMatch': 0
-                }
-                result.push(resultIntent);
-            } else {
-                console.log("message:"+JSON.stringify(message));
-                var mots = message.split(" ");
-                nbMot = 0;
-                //Pour chaque mot du message regarder s'il est présent dans l'exemple
-                intent.examples.forEach((example, ind, arr) => {
-                    mots.forEach((mot, indMot, arrayMots) => {
-                         //console.log("example:"+JSON.stringify(example)+ 'mot='+mot);   
-                        if (example.indexOf(mot) > 0) {
-                            nbMot++
-                        }
-                    });
-                });
-                if (nbMot > 0) {
+    db.intents.find({}, function (err, docs) {
+
+
+        docs.forEach((doc, index, array) => {
+            console.log('doc=' + JSON.stringify(doc))
+            doc.examples.forEach((example, ind, arr) => {
+                console.log('example=' + example);
+                if (example == message) {
                     var resultIntent = {
-                        'intent': intent,
-                        'exact': false,
-                        'nbWordsMatch': nbMot
+                        'intent': doc.intent,
+                        'exact': true,
+                        'nbWordsMatch': 0
                     }
                     result.push(resultIntent);
+                } else {
+                    console.log("message:" + JSON.stringify(message));
+                    var mots = message.split(" ");
                     nbMot = 0;
+                    //Pour chaque mot du message regarder s'il est présent dans l'exemple
+                    doc.examples.forEach((example, ind, arr) => {
+                        mots.forEach((mot, indMot, arrayMots) => {
+                            //console.log("example:"+JSON.stringify(example)+ 'mot='+mot);   
+                            if (example.indexOf(mot) > 0) {
+                                nbMot++
+                            }
+                        });
+                    });
+                    if (nbMot > 0) {
+                        var resultIntent = {
+                            'intent': intent,
+                            'exact': false,
+                            'nbWordsMatch': nbMot
+                        }
+                        result.push(resultIntent);
+                        nbMot = 0;
+                    }
                 }
-            }
+            });
         });
+
+        // extract intent
+        let finalIntent = null;
+        finalIntent = result.filter((intent, ind, array) => exact = true);
+        if (!finalIntent && result.length > 0) {
+            finalIntent = Math.max.apply(Math, result.map(function (o) { return o.nbWordsMatch; }))
+        }
+        console.log('checkIntent =' + JSON.stringify(finalIntent + '.'));
+        if (finalIntent != null && finalIntent.length > 0) {
+            console.log("finalIntent " + JSON.stringify(finalIntent));
+            return finalIntent[0].intent;
+        } else {
+            return null;
+        }
     });
-    // extract intent
-    let finalIntent = null;
-    finalIntent = result.filter((intent, ind, array) => exact = true);
-    if (!finalIntent && result.length > 0 ) {
-       finalIntent = Math.max.apply(Math, result.map(function (o) { return o.nbWordsMatch; }))
-    }
-    console.log('checkIntent =' + finalIntent + '.');
-    if (finalIntent != null && finalIntent.length > 0 ) {
-        console.log("finalIntent "+JSON.stringify(finalIntent));
-        return finalIntent[0].intent;
-    } else {
-        return null;
-    }
 }
 
 controller.hears([''], 'message_received', function (bot, message) {
     let intent = checkIntent(message.text);
+    console.log('intent after check='+intent);
     if (intent == null || intent.length == 0) {
         bot.startConversation(message, function (err, convo) {
             if (!err) {
@@ -139,7 +153,10 @@ controller.hears([''], 'message_received', function (bot, message) {
                         'intent': response.text,
                         'examples': examples
                     }
-                    intents.push(intent);
+                    //intents.push(intent);
+                    db.intents.insert(intent, function (err, newDocs) {
+
+                    })
                     convo.say('Cool i have learn a new intent!');
                     convo.next();
                 });
@@ -147,7 +164,7 @@ controller.hears([''], 'message_received', function (bot, message) {
         });
     } else {
         console.log("intent trouve" + JSON.stringify(intent))
-        bot.reply(message, 'Ok i anderstand this intent : ' + intent.intent);        
+        bot.reply(message, 'Ok i anderstand this intent : ' + intent.intent);
         //TODO Si l'intent n'a pas été trouve avec une correspondance exact on peut enregistrer le message comme example pour cette intention
 
         //TODO Si l'action de l'intention necessite des paramètres il faut entamer un dialog
@@ -156,7 +173,7 @@ controller.hears([''], 'message_received', function (bot, message) {
         // - exemple : prendre un rendez vous. Il faut le jour, l'heure, avec qui.
         // - exemple : lister les rendez a venir. Il ne faut aucun paramètre
         // *** Faut il associer un verbe à chaque intention? ex : ajouter, supprimer, lister, rechercher, voir... ***
-        
+
         //findDialog(intent);
     }
 });
